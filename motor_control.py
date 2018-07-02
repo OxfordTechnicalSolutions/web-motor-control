2bb7cfdaf043a55299a47c14e806fc820fa72f05
Richard Sands <rsands@oxts.com>
2018-07-02 13:46:26 +0000

    graphics in place for webapp with css

--- motor_control.py
+++ motor_control.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask import request
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 
@@ -7,12 +7,16 @@ import atexit
 
 app = Flask(__name__)
 
-mh = Adafruit_MotorHAT(addr=0x60)
-myMotor = mh.getMotor(3)
-myMotor.setSpeed(150)
-myMotor.run(Adafruit_MotorHAT.FORWARD)
-myMotor.run(Adafruit_MotorHAT.RELEASE)
-
+try:
+  mh = Adafruit_MotorHAT(addr=0x60)
+  myMotor = mh.getMotor(3)
+  myMotor.setSpeed(150)
+  myMotor.run(Adafruit_MotorHAT.FORWARD)
+  myMotor.run(Adafruit_MotorHAT.RELEASE)
+except:
+  # TODO: notify user of error nicely
+  pass
+  
 
 def turnOffMotors():
     mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
@@ -27,22 +31,34 @@ def web_interface():
   html = open("web_interface.html")
   response = html.read().replace('\n', '')
   html.close()
-  myMotor.setSpeed(0)
+  try:
+    myMotor.setSpeed(0)
+  except:
+  # TODO: notify user of error nicely
+    pass
   return response
 
 @app.route("/set_speed")
 def set_speed():
-  speed = request.args.get("speed")
-  print "Received " + str(speed)
 
-  direction = Adafruit_MotorHAT.FORWARD
-  if int(speed) < 0:
-    direction = Adafruit_MotorHAT.BACKWARD
+  try:
+    speed = request.args.get("speed")
+    print "Received " + str(speed)
 
-  myMotor.run(direction)
-  myMotor.setSpeed(abs(int(speed)))
+    direction = Adafruit_MotorHAT.FORWARD
+    if int(speed) < 0:
+      direction = Adafruit_MotorHAT.BACKWARD
+
+    myMotor.run(direction)
+    myMotor.setSpeed(abs(int(speed)))
+  except:
+    return "Error!"
 
   return "Received " + str(speed)
+
+@app.route('/files/<path:path>')
+def send_file(path):
+    return send_from_directory('files', path)
 
 def main():
   app.run(host= '0.0.0.0')
