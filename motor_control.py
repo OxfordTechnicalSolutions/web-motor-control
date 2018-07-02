02305767c18613c07b0a26f714370d7ed8df2568
Richard Sands <rsands@oxts.com>
2018-07-02 15:38:55 +0000

    dual dc motor control with simple sliders

--- motor_control.py
+++ motor_control.py
@@ -4,15 +4,27 @@ from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 
 import time
 import atexit
+# import threading
+# import random
 
 app = Flask(__name__)
 
+# create empty threads (these will hold the stepper 1 and 2 threads)
+# tL = threading.Thread()
+# tR = threading.Thread()
+
 try:
   mh = Adafruit_MotorHAT(addr=0x60)
-  myMotor = mh.getMotor(3)
-  myMotor.setSpeed(150)
-  myMotor.run(Adafruit_MotorHAT.FORWARD)
-  myMotor.run(Adafruit_MotorHAT.RELEASE)
+  # left motor
+  myMotorL = mh.getMotor(4)
+  myMotorL.setSpeed(150)
+  myMotorL.run(Adafruit_MotorHAT.FORWARD)
+  myMotorL.run(Adafruit_MotorHAT.RELEASE)
+  # right motor
+  myMotorR = mh.getMotor(3)
+  myMotorR.setSpeed(150)
+  myMotorR.run(Adafruit_MotorHAT.FORWARD)
+  myMotorR.run(Adafruit_MotorHAT.RELEASE)
 except:
   # TODO: notify user of error nicely
   pass
@@ -26,13 +38,29 @@ def turnOffMotors():
 
 atexit.register(turnOffMotors)
 
+def dc_worker(motor, direction, speed):
+  motor.run(direction)
+  motor.setSpeed(abs(int(speed)))
+
+def dc_set_speed(motor, speed):
+  direction = Adafruit_MotorHAT.FORWARD
+  if int(speed) < 0:
+    direction = Adafruit_MotorHAT.BACKWARD
+  motor.run(direction)
+  motor.setSpeed(abs(int(speed)))
+
+  # t = threading.Thread(target=dc_worker, args=(motor, direction, speed))
+  # t.start()
+
 @app.route("/")
 def web_interface():
-  html = open("web_interface.html")
+  # html = open("web_interface.html")
+  html = open("web_interface_simple_m3.html")
   response = html.read().replace('\n', '')
   html.close()
   try:
-    myMotor.setSpeed(0)
+    myMotorL.setSpeed(0)
+    myMotorR.setSpeed(0)
   except:
   # TODO: notify user of error nicely
     pass
@@ -41,20 +69,17 @@ def web_interface():
 @app.route("/set_speed")
 def set_speed():
 
-  try:
-    speed = request.args.get("speed")
-    print "Received " + str(speed)
+  # try:
+  left_speed = request.args.get("left_speed")
+  right_speed = request.args.get("right_speed")
+  print "Received " + str(left_speed) + " " + str(right_speed)
 
-    direction = Adafruit_MotorHAT.FORWARD
-    if int(speed) < 0:
-      direction = Adafruit_MotorHAT.BACKWARD
+  dc_set_speed(myMotorL, left_speed)
+  dc_set_speed(myMotorR, right_speed)
+  # except:
+  #   return "Error!"
 
-    myMotor.run(direction)
-    myMotor.setSpeed(abs(int(speed)))
-  except:
-    return "Error!"
-
-  return "Received " + str(speed)
+  return "Received " + str(left_speed) + " " + str(right_speed)
 
 @app.route('/files/<path:path>')
 def send_file(path):
